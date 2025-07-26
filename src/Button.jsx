e1617a701a6b609480ee738137ea22b8efb0af2d
Joshua <noll.joshua@outlook.com>
2025-07-26 14:30:24 -0500

    add Button component

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 
 function Button() {
-  
+  const [inFavorites, setInFavorites] = useState(false);
+
+  function addToFavorites(){
+    inFavorites ? setInFavorites(false) : setInFavorites(true);
+    console.log(inFavorites)
+  }
+
+  return (
+    <button onClick={addToFavorites}>Add to favorites!</button>
+  )
 }
-
-
-
 
 export default Button;
--- src/List.jsx
+++ src/List.jsx
@@ -1,59 +1,54 @@
 import { useState, useEffect } from "react";
-
+import Button from './Button.jsx'
 
 function List() {
-  const [list, setlist] = useState([])
+  const [drinkList, setList] = useState([])
 
-    useEffect(() =>{
-      fetch('http://localhost:3001')
-        .then((Res) => Res.json())
-        .then(data =>        // setlist(data.drinks.strDrink)
-        setlist(data.drinks.map((item) => {
+  useEffect(() => {
+    fetch('http://localhost:3001')
+      .then((result) => result.json())
+      .then(data => setList(data.drinks.map((item) => {
           let itemObj = {}
           itemObj.name = item.strDrink;
           itemObj.img = item.strDrinkThumb;
           return itemObj;
         })))
-    }, [])
-  
-
-    if(list.length < 1) return 'loading...'
-    else if(list.length >=10) {
-
-    
-    return(
-        <>
-          <ul id="cocktaillist">
-            {list.map((list) => <li key={list.name} >
-                                  <label>{list.name}</label>
-                                  <br/>
-                                  <img src={list.img} alt="Image" />
-                                </li>)}
-          </ul>
-        </>
-      )
-      }
-    }
+  }, [])
 
 
-  
+  if (drinkList.length < 1) return 'loading drink list..'
+  else if (drinkList.length >= 10) {
+    return (
+      <>
+        <ul id="cocktaillist">
+          {drinkList.map((drink) =>
+          <li key={drink.name}>
+            <label for={drink.name}>{drink.name}</label>
+            <br/>
+            <img name={drink.name} src={drink.img} alt={drink.name} />
+            <Button></Button>
+          </li>
+          )}
+        </ul>
+      </>
+    )
+  }
+}
 
 export default List;
 
 
-
-
-    // for( let i = 0; i < list.length; i++){
-    //   let item = document.createElement('li');
-    //   let imgTarget = list[i].img;
-    //   let imgElement = document.createElement('img');
-    //   let breaks = document.createElement('br')
-    //   imgElement.src = imgTarget;
-    //   item.innerHTML = list[i].name;
-    //   item.appendChild(breaks);
-    //   item.appendChild(imgElement);
-    //   document.getElementById('cocktaillist').appendChild(item);
-    //   // console.log(list[i].name, list[i].img)
-    //   // let vars = list[i];
-    //   // console.log(vars.name, vars.img)
-    // }
+// for( let i = 0; i < list.length; i++){
+//   let item = document.createElement('li');
+//   let imgTarget = list[i].img;
+//   let imgElement = document.createElement('img');
+//   let breaks = document.createElement('br')
+//   imgElement.src = imgTarget;
+//   item.innerHTML = list[i].name;
+//   item.appendChild(breaks);
+//   item.appendChild(imgElement);
+//   document.getElementById('cocktaillist').appendChild(item);
+//   // console.log(list[i].name, list[i].img)
+//   // let vars = list[i];
+//   // console.log(vars.name, vars.img)
+// }
